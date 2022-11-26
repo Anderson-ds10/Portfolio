0a5f924fa2d021a17204332352968b877c97f70c
Anderson <marquesandersonds@gmail.com>
2022-11-25 21:56:11 -0300

    Projeto portifolio

---
+++ formacao-efeito.js
@@ -0,0 +1,41 @@
+function mostrarTextoADS() {
+    document.querySelector(".formacao-text--ads").innerText = "Análise e Desenvolvimento de Sistemas";
+}
+function retirarTextoADS() {
+    document.querySelector(".formacao-text--ads").innerText = ''
+}
+
+function mostrarTextoFrontEnd() {
+    document.querySelector(".formacao-text--frontend").innerText = "Desenvolvimento Front-End";
+}
+function retirarTextoFrontEnd() {
+    document.querySelector(".formacao-text--frontend").innerText = ''
+}
+
+function mostrarTextoAgility() {
+    document.querySelector(".formacao-text--agility").innerText = "Business Agility";
+}
+function retirarTextoAgility() {
+    document.querySelector(".formacao-text--agility").innerText = ''
+}
+
+function mostrarTextoPessoal() {
+    document.querySelector(".formacao-text--pessoal").innerText = "Desenvolvimento Pessoal";
+}
+function retirarTextoPessoal() {
+    document.querySelector(".formacao-text--pessoal").innerText = ''
+}
+
+function mostrarTextoQuimica() {
+    document.querySelector(".formacao-text--quimica").innerText = "Técnico em Química";
+}
+function retirarTextoQuimica() {
+    document.querySelector(".formacao-text--quimica").innerText = ''
+}
+
+function mostrarTextoEnfermagem() {
+    document.querySelector(".formacao-text--enfermagem").innerText = "Técnico em Enfermagem";
+}
+function retirarTextoEnfermagem() {
+    document.querySelector(".formacao-text--enfermagem").innerText = ''
+}
